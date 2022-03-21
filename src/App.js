6fd0b332a75ca6a08252a31496ebb6abdb1305b0
borisljuboja <boris.ljuboj032@gmail.com>
2022-03-21 19:46:10 +0100

    Input done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Header from "./components/elements/Header";
+import Input from "./components/elements/Input";
+import { FaReact } from 'react-icons/fa';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="input-container">
+        <FaReact className="react-icon" />
+        <Input placeholder="Search projects" />
+      </div>
     </div>
   );
 }
