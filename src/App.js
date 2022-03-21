e24064cc76a96733a0dd7e37b8e566f3aeff344a
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-03-21 17:34:29 +0100

    Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+import Header from "./components/elements/Header";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
