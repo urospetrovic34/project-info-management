f309d836e294f8c7c68a53bece66302c4319de98
borisljuboja <boris.ljuboj032@gmail.com>
2022-03-30 16:29:11 +0200

    subheaderemployee done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Login } from "./components/auth/login/Login";
 import { Register } from "./components/auth/register/Register";
 import Header from "./components/elements/navigation/header/Header";
 import SubHeader from "./components/elements/navigation/subHeader/SubHeader";
+import SubHeaderEmployee from "./components/elements/navigation/subHeaderEmployee/SubHeaderEmployee";
 import Tabs from "./components/elements/tabs/Tabs";
 import { Test } from "./components/test/Test";
 import { useAuth } from "./contexts/AuthProvider";
@@ -12,65 +13,67 @@ import { DefaultRoute } from "./routes/defaultRoute";
 import { PrivateRoute } from "./routes/privateRoute";
 import { PublicRoute } from "./routes/publicRoute";
 
+
 function App() {
-  const [authState, authDispatch] = useAuth();
-  const { user, token } = authState;
+    const [authState, authDispatch] = useAuth();
+    const { user, token } = authState;
 
-  window.addEventListener("beforeunload", () => {
-    if (localStorage.getItem("remember") === "false") {
-      localStorage.clear();
-    }
-  });
+    window.addEventListener("beforeunload", () => {
+        if (localStorage.getItem("remember") === "false") {
+            localStorage.clear();
+        }
+    });
 
-  return (
-    <Router>
-      <div className="wrapper">
-        {user && token && <Header />}
-        {/* {user && token && <Tabs />} */}
+    return (
+        <Router>
+            <div className="wrapper">
+                {user && token && <Header />}
+                {/* {user && token && <Tabs />} */}
 
-        {/* {token && <SubHeader />} */}
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute redirect="/login">
-                <Test />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/admin"
-            element={
-              <PrivateRoute redirect="/login">
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/login"
-            element={
-              <PublicRoute redirect="/">
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            exact
-            path="/register"
-            element={
-              <PublicRoute redirect="/">
-                <Register />
-              </PublicRoute>
-            }
-          />
-          <Route exact path="*" element={<DefaultRoute></DefaultRoute>} />
-        </Routes>
-      </div>
-    </Router>
-  );
+                {/* {token && <SubHeader />} */}
+                <SubHeaderEmployee />
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <PrivateRoute redirect="/login">
+                                <Test />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/admin"
+                        element={
+                            <PrivateRoute redirect="/login">
+                                <Dashboard />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/login"
+                        element={
+                            <PublicRoute redirect="/">
+                                <Login />
+                            </PublicRoute>
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/register"
+                        element={
+                            <PublicRoute redirect="/">
+                                <Register />
+                            </PublicRoute>
+                        }
+                    />
+                    <Route exact path="*" element={<DefaultRoute></DefaultRoute>} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/elements/navigation/subHeaderEmployee/SubHeaderEmployee.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SubHeaderEmployeeCSS from "../subHeaderEmployee/SubHeaderEmployee.module.css";
+import logo from "../../../../assets/q-logo.png";
+
+const subHeaderEmployee = () => {
+    return (
+        <div className={SubHeaderEmployeeCSS.container}>
+            <div className={SubHeaderEmployeeCSS.logo_container}>
+                <img src={logo} alt="logo" className={SubHeaderEmployeeCSS.logo} />
+                <div>
+                    <h2 className={SubHeaderEmployeeCSS.title}>The New Alpha</h2>
+                    <p className={SubHeaderEmployeeCSS.paragraph}>The New Alpha is a long running project. <br></br> We're creating sales pages for the clinets's business, <br /> creating blog posts, and managing the Shopify <br /> store </p>
+                </div>
+            </div>
+            <div className={SubHeaderEmployeeCSS.info}>
+                <div>
+                    <div className={SubHeaderEmployeeCSS.project_manager}>
+                        <h4>Project Manager</h4>
+                        <img src={logo} alt="logo" className={SubHeaderEmployeeCSS.profile_picture} />
+                    </div>
+                </div>
+                <div>
+                    <div className={SubHeaderEmployeeCSS.employees}>
+                        <h4>Employees</h4>
+                        <div className={SubHeaderEmployeeCSS.profile_pictures}>
+                            <img src={logo} alt="logo" className={SubHeaderEmployeeCSS.profile_picture} />
+                            <img src={logo} alt="logo" className={SubHeaderEmployeeCSS.profile_picture} />
+                            <img src={logo} alt="logo" className={SubHeaderEmployeeCSS.profile_picture} />
+                            <span className={SubHeaderEmployeeCSS.more} >+5 more</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default subHeaderEmployee
