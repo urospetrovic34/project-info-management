c729ea7243655c0f9a68aadb4f17d6dc3fce2dcc
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-03-30 13:18:51 +0200

    navigation style changed

--- src/components/elements/navigation/header/Header.js
+++ src/components/elements/navigation/header/Header.js
@@ -8,47 +8,40 @@ import { useAuth } from "../../../../contexts/AuthProvider";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const [ShowMobileNavigation, setMobileNavigation] = useState(false);
-    const [authState, authDispatch] = useAuth();
-    const { user } = authState;
+  const [ShowMobileNavigation, setMobileNavigation] = useState(false);
+  const [authState, authDispatch] = useAuth();
+  const { user } = authState;
 
-    return (
-        <div className={HeaderCSS.header}>
-            <div className={HeaderCSS.logoHeaderContainer}>
-                <img src={logo} alt="logo" className={HeaderCSS.logo} />
-                <h1 className={HeaderCSS.platformName}>Q Project Info</h1>
-            </div>
+  return (
+    <div className={HeaderCSS.header}>
+      <div className={HeaderCSS.logoHeaderContainer}>
+        <img src={logo} alt="logo" className={HeaderCSS.logo} />
+        <h1 className={HeaderCSS.platformName}>Q Project Info</h1>
+      </div>
 
-            <div className={HeaderCSS.navigationContainer}>
-                {/* {user.role.name === "System Administrator" && <span className={HeaderCSS.navigationContent}>Dashboard</span>} */}
-                {user.role.name === "Authenticated" && (
-                    <Link to="/admin">
-                        <span className={HeaderCSS.navigationContent}>
-                            Dashboard
-                        </span>
-                    </Link>
-                )}
-                <span className={HeaderCSS.navigationContent}>My Projects</span>
-                <span className={HeaderCSS.navigationContent}>Account</span>
-                <span
-                    className={HeaderCSS.navigationContent}
-                    onClick={() => AuthAPI.logout(authDispatch)}
-                >
-                    Logout
-                </span>
-            </div>
+      <div className={HeaderCSS.navigationContainer}>
+        {/* {user.role.name === "System Administrator" && <span className={HeaderCSS.navigationContent}>Dashboard</span>} */}
+        {user.role.name === "Authenticated" && (
+          <Link to="/admin" className={HeaderCSS.navigationContent}>
+            <span>Dashboard</span>
+          </Link>
+        )}
+        <span className={HeaderCSS.navigationContent}>My Projects</span>
+        <span className={HeaderCSS.navigationContent}>Account</span>
+        <span className={HeaderCSS.navigationContent} onClick={() => AuthAPI.logout(authDispatch)}>
+          Logout
+        </span>
+      </div>
 
-            <img
-                src={hamburgerIcon}
-                alt="icon-hamburger"
-                className={HeaderCSS.hamburgerIcon}
-                onClick={() => setMobileNavigation(true)}
-            />
-            {ShowMobileNavigation ? (
-                <NavigationMob showMobileNav={setMobileNavigation} />
-            ) : null}
-        </div>
-    );
+      <img
+        src={hamburgerIcon}
+        alt="icon-hamburger"
+        className={HeaderCSS.hamburgerIcon}
+        onClick={() => setMobileNavigation(true)}
+      />
+      {ShowMobileNavigation ? <NavigationMob showMobileNav={setMobileNavigation} /> : null}
+    </div>
+  );
 };
 
 export default Header;
