1eaf67ab1a0d4ced0fe28931eb48a758268f7b77
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-04-15 13:58:27 +0200

    user parameters position changed

--- src/hooks/query/user.js
+++ src/hooks/query/user.js
@@ -3,57 +3,57 @@ import { queryClient } from "../..";
 import UserAPI from "../../actions/user";
 
 const useUsers = () => {
-    return useQuery("users", () => UserAPI.get());
+  return useQuery("users", () => UserAPI.get());
 };
 
 const useSingleUser = (id) => {
-    return useQuery(["user", id], () => UserAPI.getOne(id));
+  return useQuery(["user", id], () => UserAPI.getOne(id));
 };
 
 const useCurrentUser = (id) => {
-    return useQuery("userCurrent", UserAPI.getCurrent);
+  return useQuery("userCurrent", UserAPI.getCurrent);
 };
 
 //I don't think it was necessary to make this one into a hook function,
 //but whatever
 
 const useCountUsers = () => {
-    return useQuery("userCount", UserAPI.count);
+  return useQuery("userCount", UserAPI.count);
 };
 
-const useEditUserMutation = (id, data) => {
-    return useMutation(
-        () => {
-            return UserAPI.edit(id, data);
-        },
-        {
-            onSettled: () => {
-                queryClient.invalidateQueries("users");
-            },
-        }
-    );
+const useEditUserMutation = () => {
+  return useMutation(
+    (id, data) => {
+      return UserAPI.edit(id, data);
+    },
+    {
+      onSettled: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
 };
 
-const useDeleteUserMutation = (id) => {
-    return useMutation(
-        () => {
-            return UserAPI.remove(id);
-        },
-        {
-            onSettled: () => {
-                queryClient.invalidateQueries("users");
-            },
-        }
-    );
+const useDeleteUserMutation = () => {
+  return useMutation(
+    (id) => {
+      return UserAPI.remove(id);
+    },
+    {
+      onSettled: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
 };
 
 const user = {
-    useUsers,
-    useSingleUser,
-    useCurrentUser,
-    useCountUsers,
-    useEditUserMutation,
-    useDeleteUserMutation,
+  useUsers,
+  useSingleUser,
+  useCurrentUser,
+  useCountUsers,
+  useEditUserMutation,
+  useDeleteUserMutation,
 };
 
 export default user;
