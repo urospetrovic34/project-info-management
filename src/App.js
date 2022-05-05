6324fc1e75a060800290fadfa7499a500b8e8920
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-05-05 18:27:38 +0200

    app.js issuee fixed

--- src/App.js
+++ src/App.js
@@ -22,11 +22,9 @@ import CreateProjectPage from "./components/createProject/CreateProjectPage";
 import EditAccountPage from "./components/elements/editAccountPage/EditAccountPage";
 import SystemAdminPage from "./components/systemAdminPage/SystemAdminPage";
 
-
 function App() {
   const [authState, authDispatch] = useAuth();
   const { user, token } = authState;
-
 
   return (
     <Router>
@@ -106,103 +104,6 @@ function App() {
       </div>
     </Router>
   );
-
-  useEffect(() => {
-    document.title = "Project Info Management";
-  }, []);
-
-  return (
-    <Router>
-      <div className="wrapper">
-        {user && token && <Header />}
-        {/* <EditAccountPage /> */}
-        {/* {user && token && <Tabs />} */}
-        {/* <CreateNoteTab /> */}
-        {/* {token && <SubHeader />} */}
-        {/* <SubHeaderEmployee /> */}
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute redirect="/login">
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/projects/create"
-            element={
-              <PrivateRoute redirect="/login">
-                <CreateProjectPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/projects/:id"
-            element={
-              <PrivateRoute redirect="/login">
-                <Project />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/notes/create/:id"
-            element={
-              <PrivateRoute redirect="/login">
-                <CreateNoteTab />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/account"
-            element={
-              <PrivateRoute redirect="/login">
-                <EditAccountPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/admin"
-            element={
-              <PrivateRoute redirect="/login">
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            exact
-            path="/login"
-            element={
-              <PublicRoute redirect="/">
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            exact
-            path="/register"
-            element={
-              <PublicRoute redirect="/">
-                <Register />
-              </PublicRoute>
-            }
-          />
-          <Route
-            exact
-            path="*"
-            element={<DefaultRoute></DefaultRoute>}
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
-
 }
 
 export default App;
