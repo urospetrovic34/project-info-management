7911c2b8599614538081264d003394726c8597ab
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-05-12 17:30:32 +0200

    System Admin page styled and responsive

--- src/components/systemAdminPage/SystemAdminPage.js
+++ src/components/systemAdminPage/SystemAdminPage.js
@@ -12,97 +12,77 @@ import useDebounce from "../../hooks/custom/useDebounce";
 import user from "../../hooks/query/user";
 
 const SystemAdminPage = () => {
-    const [pageNumber, setPageNumber] = useState(0);
-    const [userName, setUserName] = useState(null);
-    const debouncedPageNumber = useDebounce(pageNumber, 250);
-    const debouncedUserName = useDebounce(userName, 1000);
-    const users = userHooks.useUsersV2(
-        null,
-        debouncedUserName,
-        debouncedPageNumber
-    );
-    console.log(pageNumber);
-    const sortEmployeeOptions = [
-        { value: "", label: "Option-1" },
-        { value: "", label: "Option-2" },
-        { value: "", label: "Option-3" },
-        { value: "", label: "Option-4" },
-    ];
-    console.log(users);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [userName, setUserName] = useState(null);
+  const debouncedPageNumber = useDebounce(pageNumber, 250);
+  const debouncedUserName = useDebounce(userName, 1000);
+  const users = userHooks.useUsersV2(null, debouncedUserName, debouncedPageNumber);
+  console.log(pageNumber);
+  const sortEmployeeOptions = [
+    { value: "", label: "Option-1" },
+    { value: "", label: "Option-2" },
+    { value: "", label: "Option-3" },
+    { value: "", label: "Option-4" },
+  ];
+  console.log(users);
 
-    const handlePageChange = async (event) => {
-        setPageNumber(event.target.value - 1);
-    };
+  const handlePageChange = async (event) => {
+    setPageNumber(event.target.value - 1);
+  };
 
-    const handleNextPageChange = async () => {
-        setPageNumber(pageNumber + 1);
-    };
+  const handleNextPageChange = async () => {
+    setPageNumber(pageNumber + 1);
+  };
 
-    const handlePreviousPageChange = async () => {
-        setPageNumber(pageNumber - 1);
-    };
+  const handlePreviousPageChange = async () => {
+    setPageNumber(pageNumber - 1);
+  };
 
-    const handleUserNameFilter = async (event) => {
-        setPageNumber(0);
-        setUserName(event.target.value);
-    };
+  const handleUserNameFilter = async (event) => {
+    setPageNumber(0);
+    setUserName(event.target.value);
+  };
 
-    useEffect(() => {
-        if (
-            users.data?.data?.length === 0 &&
-            users.data?.meta?.pagination.page > 1
-        ) {
-            setPageNumber(0);
-        }
-    }, [users]);
+  useEffect(() => {
+    if (users.data?.data?.length === 0 && users.data?.meta?.pagination.page > 1) {
+      setPageNumber(0);
+    }
+  }, [users]);
 
-    return (
-        <div className={SystemAdminPageCSS.container}>
-            <>
-                <div className={SystemAdminPageCSS.flex}>
-                    <div className={SystemAdminPageCSS.search_container}>
-                        <div>
-                            <Input
-                                placeholder={"Search users..."}
-                                onChange={handleUserNameFilter}
-                            />
-                        </div>
-                        <div className={SystemAdminPageCSS.select}>
-                            <Select
-                                placeholder={"Sort by..."}
-                                options={sortEmployeeOptions}
-                                multi={false}
-                                isSearchable={false}
-                            />
-                        </div>
-                    </div>
-                    <div className={SystemAdminPageCSS.btn}>
-                        <Link to="/users/create">
-                            <Button text=" + Add User" />
-                        </Link>
-                    </div>
-                </div>
-                <div className={SystemAdminPageCSS.card_container}>
-                    {users.status === "success" &&
-                        users.data?.data?.map((user) => (
-                            <UserCard key={user.id} user={user} />
-                        ))}
-                </div>
-                <div className={SystemAdminPageCSS.pagination}>
-                    {users.status === "success" && (
-                        <Pagination
-                            currentPage={users.data?.meta?.pagination.page}
-                            totalCount={users.data?.meta?.pagination.total}
-                            pageSize={users.data?.meta?.pagination.pageSize}
-                            handlePageChange={handlePageChange}
-                            handleNextPageChange={handleNextPageChange}
-                            handlePreviousPageChange={handlePreviousPageChange}
-                        />
-                    )}
-                </div>
-            </>
+  return (
+    <div className={SystemAdminPageCSS.container}>
+      <div className={SystemAdminPageCSS.flex}>
+        <div className={SystemAdminPageCSS.search_container}>
+          <div>
+            <Input placeholder={"Search users..."} onChange={handleUserNameFilter} />
+          </div>
+          <div className={SystemAdminPageCSS.select}>
+            <Select placeholder={"Sort by..."} options={sortEmployeeOptions} multi={false} isSearchable={false} />
+          </div>
         </div>
-    );
+        <div className={SystemAdminPageCSS.btn}>
+          <Link to="/users/create">
+            <Button text=" + Add User" />
+          </Link>
+        </div>
+      </div>
+      <div className={SystemAdminPageCSS.card_container}>
+        {users.status === "success" && users.data?.data?.map((user) => <UserCard key={user.id} user={user} />)}
+      </div>
+      <div className={SystemAdminPageCSS.pagination}>
+        {users.status === "success" && (
+          <Pagination
+            currentPage={users.data?.meta?.pagination.page}
+            totalCount={users.data?.meta?.pagination.total}
+            pageSize={users.data?.meta?.pagination.pageSize}
+            handlePageChange={handlePageChange}
+            handleNextPageChange={handleNextPageChange}
+            handlePreviousPageChange={handlePreviousPageChange}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SystemAdminPage;
