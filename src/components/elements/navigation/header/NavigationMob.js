5daff42d97d1f6ee3676703bde5e67ae03d77049
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-05-13 11:25:34 +0200

    mobile navigation linked

--- src/components/elements/navigation/header/NavigationMob.js
+++ src/components/elements/navigation/header/NavigationMob.js
@@ -3,11 +3,18 @@ import HeaderCSS from "../header/Header.module.css";
 import closeIcon from "../../../../assets/icon-close-modal.svg";
 import Avatar from "../../../../assets/user.png";
 import { useAuth } from "../../../../contexts/AuthProvider";
+import { Link } from "react-router-dom";
+import AuthAPI from "../../../../actions/auth";
 
 const NavigationMob = ({ showMobileNav }) => {
   const [authState, authDispatch] = useAuth();
   const { user } = authState;
   console.log(user);
+  const handleLogout = () => {
+    console.log(user);
+    AuthAPI.logout(authDispatch);
+    console.log(user);
+  };
   return (
     <div id="myNav" className={HeaderCSS.overlay}>
       <span className={HeaderCSS.closebtn}>
@@ -21,9 +28,22 @@ const NavigationMob = ({ showMobileNav }) => {
         </div>
       </div>
       <div className={HeaderCSS.overlayContent}>
-        <a href="#myProject">My Projects</a>
-        <a href="#account">Account</a>
-        <a href="#logout">Logout</a>
+        {user.role.name === "System Administrator" && (
+          <Link to="/admin/users" className={HeaderCSS.navigationContent}>
+            <span onClick={() => showMobileNav(false)}>Dashboard</span>
+          </Link>
+        )}
+        {user.role.name !== "System Administrator" && (
+          <Link to="/" className={HeaderCSS.navigationContent}>
+            <span onClick={() => showMobileNav(false)}>My Projects</span>
+          </Link>
+        )}
+        <Link to="/account" className={HeaderCSS.navigationContent}>
+          <span onClick={() => showMobileNav(false)}>Account</span>
+        </Link>
+        <span onClick={handleLogout}>
+          <a href="#">Logout</a>
+        </span>
       </div>
     </div>
   );
